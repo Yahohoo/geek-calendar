572bc7887af6a0af8aaf0d155fb203932be4d9ab
Timur <thrillofground@gmail.com>
2019-08-18 06:20:39 +0300

    Добавлены отступы, исправлена ошибка с json, удален лишний css

--- src/App.vue
+++ src/App.vue
@@ -65,7 +65,8 @@ export default {
   mounted() {
     const { href } = window.location
     const params = new window.URL(href).searchParams
-    const state = JSON.parse(decompress(params.get('__sched_state')))
+    const schedStateParam = params.get('__sched_state')
+    const state = schedStateParam && JSON.parse(decompress(schedStateParam))
 
     if (state && state.filters) {
       this.updateFilters({ filters: state.filters })
@@ -99,7 +100,12 @@ body {
   font-family: 'Ubuntu', sans-serif;
   color: #212121;
   margin: auto;
-  padding: 5px;
+}
+
+@media (min-width: 600px) {
+  .calendar-app {
+    padding: 0 50px;
+  }
 }
 
 $themes: (
@@ -129,148 +135,5 @@ $themes: (
   cursor: pointer;
   padding: 10px;
   font-weight: bold;
-}
-</style>
-
-<style lang=scss>
-// загрузка в сетке
-.lds-roller {
-  display: inline-block;
-  position: relative;
-  width: 64px;
-  height: 64px;
-}
-
-.lds-roller div {
-  animation: lds-roller 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
-  transform-origin: 32px 32px;
-}
-
-.lds-roller div:after {
-  content: " ";
-  display: block;
-  position: absolute;
-  width: 6px;
-  height: 6px;
-  border-radius: 50%;
-  background: lightblue;
-  margin: -3px 0 0 -3px;
-}
-
-.lds-roller div:nth-child(1) {
-  animation-delay: -0.036s;
-}
-
-.lds-roller div:nth-child(1):after {
-  top: 50px;
-  left: 50px;
-}
-
-.lds-roller div:nth-child(2) {
-  animation-delay: -0.072s;
-}
-
-.lds-roller div:nth-child(2):after {
-  top: 54px;
-  left: 45px;
-}
-
-.lds-roller div:nth-child(3) {
-  animation-delay: -0.108s;
-}
-
-.lds-roller div:nth-child(3):after {
-  top: 57px;
-  left: 39px;
-}
-
-.lds-roller div:nth-child(4) {
-  animation-delay: -0.144s;
-}
-
-.lds-roller div:nth-child(4):after {
-  top: 58px;
-  left: 32px;
-}
-
-.lds-roller div:nth-child(5) {
-  animation-delay: -0.18s;
-}
-
-.lds-roller div:nth-child(5):after {
-  top: 57px;
-  left: 25px;
-}
-
-.lds-roller div:nth-child(6) {
-  animation-delay: -0.216s;
-}
-
-.lds-roller div:nth-child(6):after {
-  top: 54px;
-  left: 19px;
-}
-
-.lds-roller div:nth-child(7) {
-  animation-delay: -0.252s;
-}
-
-.lds-roller div:nth-child(7):after {
-  top: 50px;
-  left: 14px;
-}
-
-.lds-roller div:nth-child(8) {
-  animation-delay: -0.288s;
-}
-
-.lds-roller div:nth-child(8):after {
-  top: 45px;
-  left: 10px;
-}
-
-@keyframes lds-roller {
-  0% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(360deg);
-  }
-}
-// запись активна
-.lds-ripple {
-  display: inline-block;
-  position: relative;
-  width: 25px;
-  height: 25px;
-}
-
-.lds-ripple div {
-  position: absolute;
-  border: 2px solid lightblue;
-  opacity: 1;
-  border-radius: 50%;
-  animation: lds-ripple 1s cubic-bezier(0, 0.2, 0.8, 1) infinite;
-}
-
-.lds-ripple div:nth-child(2) {
-  animation-delay: -0.5s;
-}
-
-@keyframes lds-ripple {
-  0% {
-    top: 10px;
-    left: 10px;
-    width: 0;
-    height: 0;
-    opacity: 1;
-  }
-  100% {
-    top: 0px;
-    left: 0px;
-    width: 20px;
-    height: 20px;
-    opacity: 0;
-  }
 }
 </style>
